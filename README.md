8ebf982bc74fa0f63d6d857a416907b2318246e5
Plasmatium <slv309@163.com>
2016-07-06 22:24:25 +0800

    Add protocol.py and put some code <class NoInheritMeta>

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # pyprotocol
+
+    protocol lib for python
+    usage:
+    >>>
+    >>> @protocol
+    ... class protocol_1():
+    ...     def get_class_name(self):
+    ...         return self.__class__.__name__
+    ...
+    >>> @extension(protocol_1)
+    ... class MyClass():
+    ...     pass
+    ...
+    >>> a = MyClass()
+    >>> a.get_class_name()
+    'MyClass'
+
+    >>>
+    >>> from hashlib import MD5
+    >>> from time import ctime
+    >>>
+    >>> @protocol
+    ... class protocol_2(ParentClass):
+    ...     def func(self, *args, **kwargs):
+    ...         pass
+    ...
---
+++ protocol.py
@@ -0,0 +1,47 @@
+'''
+    protocol lib for python
+    usage:
+    >>>
+    >>> @protocol
+    ... class protocol_1():
+    ...     def get_class_name(self):
+    ...         return self.__class__.__name__
+    ...
+    >>> @extension(protocol_1)
+    ... class MyClass():
+    ...     pass
+    ...
+    >>> a = MyClass()
+    >>> a.get_class_name()
+    'MyClass'
+
+    >>>
+    >>> from hashlib import MD5
+    >>> from time import ctime
+    >>>
+    >>> @protocol
+    ... class protocol_2(ParentClass):
+    ...     def func(self, *args, **kwargs):
+    ...         pass
+    ...
+
+    You can create protocol by inherit, but protocol could not be inherited.
+    One protocol can also contain other protocols, which means one protocol 
+    can be created under other protocols, but protocol itself can not be inherited.
+
+'''
+
+
+class InheritException(Exception):
+
+    '''Could not be inherited.'''
+
+
+class NoInheritMeta(type):
+
+    def __new__(cls, name, bases, dct):
+        for base in bases:
+            if isinstance(base, cls):
+                raise InheritException('Could not be inherited from %s' % base)
+
+        return super().__new__(cls, name, bases, dct)
